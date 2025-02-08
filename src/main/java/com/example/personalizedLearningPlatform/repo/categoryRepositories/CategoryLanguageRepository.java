3aa929c29f4dad2ab53251a9b512d036ccb8d24d
Sorana09 <sorananeculaed@gmail.com>
2025-02-08 12:57:37 +0200

    adding language services and entities into the database

--- src/main/java/com/example/personalizedLearningPlatform/repo/categoryRepositories/CategoryLanguageRepository.java
+++ src/main/java/com/example/personalizedLearningPlatform/repo/categoryRepositories/CategoryLanguageRepository.java
@@ -1,4 +1,80 @@
 package com.example.personalizedLearningPlatform.repo.categoryRepositories;
 
+import com.example.personalizedLearningPlatform.entity.CategoryEntity;
+import com.example.personalizedLearningPlatform.entity.LanguageEntity;
+import com.example.personalizedLearningPlatform.entity.UniversityEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
 public class CategoryLanguageRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void saveCatLan(Integer categoryId, Integer languageId) {
+        jdbcTemplate.update("INSERT INTO category_language (category_id, language_id) VALUES (?, ?)", categoryId, languageId);
+    }
+
+
+    public Integer findOrInserCategory(CategoryEntity categoryEntity) {
+        String selectQuery = "SELECT id FROM category_entity WHERE name = ?";
+        List<Integer> ids = jdbcTemplate.query(selectQuery,
+                new Object[]{categoryEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        }
+
+        String insertQuery = "INSERT INTO category_entity (name) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(insertQuery,
+                categoryEntity.getName(),
+
+        ids = jdbcTemplate.query(selectQuery,
+                new Object[]{categoryEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id")));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        } else {
+            throw new RuntimeException("Failed to insert or retrieve category ID");
+        }
+    }
+
+
+    public Integer findOrInsertLanguage(LanguageEntity languageEntity) {
+        String selectQuery = "SELECT id FROM language_entity WHERE name = ?";
+        List<Integer> ids = jdbcTemplate.query(selectQuery,
+                new Object[]{languageEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        }
+
+        String insertQuery = "INSERT INTO language_entity (name) VALUES (?)";
+        jdbcTemplate.update(insertQuery, languageEntity.getName());
+
+        ids = jdbcTemplate.query(selectQuery,
+                new Object[]{languageEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        } else {
+            throw new RuntimeException("Failed to insert or retrieve language ID");
+        }
+    }
+
+    public void linkCategoryWithLanguage(Integer categoryId, Integer languageId) {
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM category_language WHERE category_id = ? AND language_id = ?", new Object[]{categoryId, languageId}, Integer.class);
+
+        if (count == 0) {
+            jdbcTemplate.update("INSERT INTO category_language (category_id, language_id) VALUES (?, ?)", categoryId, languageId);
+        }
+    }
 }
--- src/main/java/com/example/personalizedLearningPlatform/repo/categoryRepositories/CategoryStudyProgramRepository.java
+++ src/main/java/com/example/personalizedLearningPlatform/repo/categoryRepositories/CategoryStudyProgramRepository.java
@@ -1,4 +1,80 @@
 package com.example.personalizedLearningPlatform.repo.categoryRepositories;
 
+import com.example.personalizedLearningPlatform.entity.CategoryEntity;
+import com.example.personalizedLearningPlatform.entity.LanguageEntity;
+import com.example.personalizedLearningPlatform.entity.StudyProgramEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
 public class CategoryStudyProgramRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void saveCatLan(Integer categoryId, Integer studyId) {
+        jdbcTemplate.update("INSERT INTO category_study_program (category_id, study_program_id) VALUES (?, ?)", categoryId, studyId);
+    }
+
+
+    public Integer findOrInserCategory(CategoryEntity categoryEntity) {
+        String selectQuery = "SELECT id FROM category_entity WHERE name = ?";
+        List<Integer> ids = jdbcTemplate.query(selectQuery,
+                new Object[]{categoryEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        }
+
+        String insertQuery = "INSERT INTO category_entity (name) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(insertQuery,
+                categoryEntity.getName(),
+
+                ids = jdbcTemplate.query(selectQuery,
+                        new Object[]{categoryEntity.getName()},
+                        (rs, rowNum) -> rs.getInt("id")));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        } else {
+            throw new RuntimeException("Failed to insert or retrieve category ID");
+        }
+    }
+
+
+    public Integer findOrInsertStudy(StudyProgramEntity studyProgramEntity) {
+        String selectQuery = "SELECT id FROM study_program_entity WHERE name = ?";
+        List<Integer> ids = jdbcTemplate.query(selectQuery,
+                new Object[]{studyProgramEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        }
+
+        String insertQuery = "INSERT INTO study_program_entity (name) VALUES (?)";
+        jdbcTemplate.update(insertQuery, studyProgramEntity.getName());
+
+        ids = jdbcTemplate.query(selectQuery,
+                new Object[]{studyProgramEntity.getName()},
+                (rs, rowNum) -> rs.getInt("id"));
+
+        if (!ids.isEmpty()) {
+            return ids.get(0);
+        } else {
+            throw new RuntimeException("Failed to insert or retrieve study program ID");
+        }
+    }
+
+    public void linkCategoryWithStudyProgram(Integer categoryId, Integer studyProgramId) {
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM category_study_program WHERE category_id = ? AND study_program_id = ?", new Object[]{categoryId, studyProgramId}, Integer.class);
+
+        if (count == 0) {
+            jdbcTemplate.update("INSERT INTO category_study_program (category_id, study_program_id) VALUES (?, ?)", categoryId, studyProgramId);
+        }
+    }
 }
